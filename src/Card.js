4d13cb234ec4980e95c4751669008d149f9ac194
Ivan Liang <ivanwenliang@gmail.com>
2019-01-24 20:32:26 -0800

    Added CardList component to show all robots

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({ name, email, id }) => {
     return (
-        <div className='bg-light-green dib br3 pa3 ma2 bw2 shadow-5 grow'>
-            <img src='https://robohash.org/testtest.png?size=200x200' alt=''/>
+        <div className='tc bg-light-green dib br3 pa3 ma2 bw2 shadow-5 grow'>
+            <img src={ `https://robohash.org/${id}.png?size=200x200` } alt=''/>
             <div>
-                <h2>Jane Doe</h2>
-                <p>jane.doe@gmail.com</p>
+                <h2>{ name }</h2>
+                <p>{ email }</p>
             </div>
         </div>
     );
